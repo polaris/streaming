acd018d580ff372c21677f4165a7907d347ee53d
Jan Deinhard <jan.deinhard@sennheiser-connect.com>
2017-04-18 17:53:46 +0200

    imprive delays.m

--- matlab/delays.m
+++ matlab/delays.m
@@ -1,18 +1,13 @@
 function result = delays(filename)
-    [pcm, samplerate] = audioread(filename);
-    t1 = pcm(:,1);
-    t2 = pcm(:,2);
+    [pcm, fs] = audioread(filename);
+    t1 = pcm(:,1); t2 = pcm(:,2);
     len = length(t1);
-    cnt = 1;
-    clicks = floor(len / samplerate);
+    clicks = floor(len / fs);
     result = zeros(1, clicks);
-    currentClick = 1;
-    disp(clicks);
-    while cnt + samplerate < len
-        disp([currentClick, clicks]);
-        delay = finddelay(t1(cnt:cnt+samplerate), t2(cnt:cnt+samplerate));
-        result(currentClick) = delay;
-        cnt = cnt + samplerate;
-        currentClick = currentClick + 1;
+    sample = 1; count = 1;
+    while sample + fs < len
+        result(count) = finddelay(t1(sample:sample+fs), t2(sample:sample+fs));
+        sample = sample + fs;
+        count = count + 1;
     end
 end
